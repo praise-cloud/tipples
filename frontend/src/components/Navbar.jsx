91188f19ad3ca1943d421702483ad005c447e3b3
Praise-cloud <georgewillpraise1@example.com>
2024-12-26 21:16:32 +0100

    made changes in the navbar.jsx file on how the mall screen should look

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 
 import { Link, NavLink } from 'react-router-dom';
 import { useState } from 'react';
-import {assets} from '../assets/frontend_assets/assets'; // Adjust the path to where your assets file is located
+import {assets, products} from '../assets/frontend_assets/assets'; // Adjust the path to where your assets file is located
 
 
 const Navbar = () => {
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
   return (
-    <div className="flex item-center justify-between py-5 font-medium">
+    <div className="flex item-center justify-between align-middle py-5 font-medium">
       <Link to="/">
         <img 
           src={assets.logo} 
@@ -18,16 +18,16 @@ const Navbar = () => {
         />
       </Link>
 
-      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-        <NavLink to="/" className="flex flex-col items-center gap-1">
+      <ul className="hidden sm:flex gap-6 text-md py-2 text-gray-700">
+        <NavLink to="/wine" className="flex flex-col items-center gap-1">
             <p>Wine</p>
             <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden"/>
         </NavLink>
-        <NavLink to="/" className="flex flex-col items-center gap-1">
+        <NavLink to="/spirit" className="flex flex-col items-center gap-1">
             <p>Spirits</p>
             <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden"/>
         </NavLink>
-        <NavLink to="/" className="flex flex-col items-center gap-1">
+        <NavLink to="/more" className="flex flex-col items-center gap-1">
             <p>More</p>
             <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden"/>
         </NavLink>
@@ -38,6 +38,7 @@ const Navbar = () => {
 
       <div className="flex items-center gap-6">
         <img 
+          onClick={()=>setVisible(true)}
           src={assets.search_icon} 
           alt=""
           className="w-5 cursor-pointer"
@@ -58,18 +59,12 @@ const Navbar = () => {
               src={assets.profile_icon} 
               alt=""
               className="w-5 cursor-pointer"
-              onClick={() => setVisible(true)}
               />
 
-              <div className="group-hover:block absolute dropdown-menu right-0 pt-4">
+              <div className="group-hover:block hidden absolute dropdown-menu left-0 pt-4">
                 <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-                  <p className="cursor-pointer hover:text-black">My Profile</p>
-                  <p className="cursor-pointer hover:text-black">Orders</p>
-                  <p className="cursor-pointer hover:text-black">Wiselist</p>
-                  <p className="cursor-pointer hover:text-black">Logout</p>
-
                   <NavLink to={"/profile"} className="cursor-pointer hover:text-black">My Profile</NavLink>
-                  <NavLink to={"/orders"} className="cursor-pointer hover:text-black">My Profile</NavLink>
+                  <NavLink to={"/orders"} className="cursor-pointer hover:text-black">Orders</NavLink>
                   <NavLink to={"/whislist"} className="cursor-pointer hover:text-black">Whislist</NavLink>
                   <NavLink to={"/"} className="cursor-pointer hover:text-black">Logout</NavLink>
                 </div>
@@ -80,24 +75,45 @@ const Navbar = () => {
             src={assets.menu_icon} 
             alt="" 
             className="w-5 cursor-pointer sm:hidden"
+            onClick={()=>setVisible(true)}
             />
       </div>
 
       {/* sidebar menu for small screen */}
-        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}`}>
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ease-in-out ${visible ? "w-full" : "w-0"}`}>
           <div className="flex flex-col text-gray-600">
-            <div onClick={()=>setVisible(false)} className="flex item-center gap-4 p-3 cursor-pointer">
+            <div onClick={()=>setVisible(false)} className="flex item-center gap-4 p-3 cursor-pointer justify-between">
+              <Link to={"/"}>
               <img 
-                src={assets.dropdown_icon} 
+                src={assets.logo} 
                 alt=""
-                className="h-4 rotate-180"
+                className="w-20"
+                
                 />
-                <p>Back</p>
+              </Link>
+              <div className="flex item-center gap-2 align-middle">
+                <img 
+                  src={assets.dropdown_icon} 
+                  alt=""
+                  className="h-4 rotate-180 mt-1"
+                  />
+                  <p>Back</p>
+              </div>
             </div>
-            <NavLink onClick={()=>setVisible(false)} className="py-5 pl-6 border" to={"/"}>Home</NavLink>
+            <div className="flex flex-col gap-2 p-3">
             <NavLink onClick={()=>setVisible(false)} className="py-5 pl-6 border" to={"/wine"}>Wine</NavLink>
             <NavLink onClick={()=>setVisible(false)} className="py-5 pl-6 border" to={"/spirit"}>Spirit</NavLink>
             <NavLink onClick={()=>setVisible(false)} className="py-5 pl-6 border" to={"/more"}>More</NavLink>
+            </div>
+
+            {/* nav image */}
+            <div className="flex justify-center">
+              <img 
+                src={products[0].image} 
+                alt=""
+                className="w-60 h-full bg-gray-700 border border-black rounded-full"
+                />
+            </div>
           </div>
         </div>
 
